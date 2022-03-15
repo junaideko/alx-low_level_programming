905a23ec37b8e7928d0110d89d47ec6175922a10
junaideko <junaidekoraimot@gmail.com>
2022-03-15 16:37:53 -0700

    function print_alphabetx10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,9 +8,10 @@ void print_alphabet_x10(void);
 {
 	int i, j;
 
-	for (i = 0; i < 11; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 'a'; j <= 'z'; j++)
+			if (i < 10 && j <= ; 'z')
 		{
 			_putchar(j);
 		}
