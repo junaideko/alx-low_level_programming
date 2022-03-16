897b7b29888d8f79afbe803fc9e28f7231131f01
junaideko <junaidekoraimot@gmail.com>
2022-03-16 03:18:07 -0700

    print_last_digit prototype

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,11 +9,11 @@ int print_last_digit(int n)
 {
 	int lastDigit;
 
-	lastDigit = n % 10;
+	lastDigit = (n % 10);
 	if (lastDigit < 0)
 	{
 		lastDigit = (-1 * lastDigit);
 	}
-	_putchar(0 + lastDigit);
+	_putchar('0' + lastDigit);
 	return (lastDigit);
 }
