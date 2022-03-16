7f190d822d4021e6d59cca666ad50a9b5b60a8b7
junaideko <junaidekoraimot@gmail.com>
2022-03-16 04:50:35 -0700

    add prototype

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - function adds two intergers and return the result
+ *  @a: is an integer
+ *  @b: is an integer
+ *
+ *  Return: result of n
+ */
+int add(int a, int b)
+{
+	int a, b;
+	int n = a + b;
+	{
+		return (n);
+	}
+}
