134b6cdc0750cf4898653321906899bbf46551d4
junaideko <junaidekoraimot@gmail.com>
2022-03-17 13:37:09 -0700

    if else

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - function mutiplies two integers.
+ * @a: first integer.
+ * @b: second integer.
+ * Return: product of a and b
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
