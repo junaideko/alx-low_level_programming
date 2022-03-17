bccbdff57cab8d8ce5302edcbee27dad3b7eefca
junaideko <junaidekoraimot@gmail.com>
2022-03-17 13:55:52 -0700

    for loop

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * print_numbers - prints numbers from 0 to 9.
+ * print_numbers - prints numbers from 0 to 9,
+ * followed by a new line.
  * Return: void
  */
 void print_numbers(void)
