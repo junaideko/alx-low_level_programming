b1af93989a1c4eadb32f82bd876683a1008609bc
junaideko <junaidekoraimot@gmail.com>
2022-03-16 21:20:19 -0700

    learning debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,7 +4,6 @@
  * main - causes an infinite loop
  * Return: 0
  */
-
 int main(void)
 {
 	int i;
@@ -13,15 +12,13 @@ int main(void)
 
 	i = 0;
 
-	while (i < 10)
-	{
-		/**
-		 * putchar(i);
-		 * i++; is required
-		 */
-	}
-
+	/**
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 * i++; required under the putchar functin
+	 */
 	printf("infinite loop avoided! \\o/\n");
-
 	return (0);
 }
