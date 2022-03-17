c1a1fef2f9151bab7f197eabee0fcc00439eb0cb
junaideko <junaidekoraimot@gmail.com>
2022-03-16 21:28:24 -0700

    learning debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - causes an infinite loop
  * Return: 0
@@ -10,6 +9,7 @@ int main(void)
 
 	printf("infinite loop incoming :(\n");
 	i = 0;
+
 	/**
 	 * while (i < 10)
 	 * {
