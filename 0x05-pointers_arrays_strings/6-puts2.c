fe3e0fc025f1530f3c876f63258d5dcb5bf495d4
junaideko <junaidekoraimot@gmail.com>
2022-03-21 20:56:28 -0700

    character of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * starting with the first character.
+ * @str: string
+ *
+ * Return: Alawys 0
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	for (; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
