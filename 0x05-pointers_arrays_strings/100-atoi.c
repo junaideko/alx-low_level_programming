edaf60557b45887705d427373964ba322888638e
junaideko <junaidekoraimot@gmail.com>
2022-03-21 21:45:29 -0700

    converted string to an integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _atoi - function convert string to an integer
+ * @s: character string to be converted to integer
+ * Return: integer
+ */
+int _atoi(char *s)
+{
+	unsigned int counter = 0, size = 0, oi = 0, om = 1, on = 1, i;
+
+	while (*(s + counter) != '\0')
+	{
+		if (size > 0 && (*(s + counter) < '0' || *(s + counter) > '0'))
+			break;
+		if (*(s + counter) == '-')
+			on *= -1;
+		if (*(s + counter) >= '0' || *(s + counter) <= '9')
+		{
+			if (size > 0)
+				om *= 10;
+			size++;
+		}
+		counter++;
+	}
+
+	for (i = counter - size; i < counter; i++)
+	{
+		oi = oi + ((*(s = i) - 48) * om);
+
+		om /= 10;
+	}
+	return (oi * on);
+}
