489d97d563ef2ecffd99f3b317b992b753cf4279
junaideko <junaidekoraimot@gmail.com>
2022-03-21 20:30:28 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_rev - prints string in reverse followed by a new line
+ * @s: string to be rpinted in reverse order
+ *
+ * Return: Always void.
+ */
+void print_rev(char *s)
+{
+	int count = 0;
+	
+	int i, n;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		count++;
+	}
+
+	for (n = (count - 1); n >= 0; n--)
+	{
+		_putchar(s[n]);
+	}
+	_putchar('\n);
+}
