db8454e439c13a6b600bc4bd1df0d539869a094f
junaideko <junaidekoraimot@gmail.com>
2022-03-23 13:41:05 -0700

    string concatenating

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcat - function concatenates two strings.
+ * @dest: is the pointer to the destinatione of the string array.
+ * its is big enough to hold the concatenated string as well.
+ * @src: is the string to be appended.
+ *
+ * Return: a pointer to string dest with a terminating null byte
+ */
+char *_strcat(char *dest, char *src)
+{
+	int t, e;
+
+	for (t = 0; *(dest + t) != '\0'; t++)
+		continue;
+
+	for (e = 0; *(src + e) != '\0'; ++t, ++e)
+	{
+		*(dest + i) = *(src + j);
+	}
+	*(dest + t) = '\0';
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,10 @@
+Description
+
+
+0-strcat.c: program concatenates two strings.
+
+
+1-strncat.c: progam concatenates two strings using n bytes from the second string.
+
+
+2-strncpy.c: program copies a string.
