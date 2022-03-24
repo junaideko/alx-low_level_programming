8458aa02eb8de78684da363accc8e442a476e81a
junaideko <junaidekoraimot@gmail.com>
2022-03-24 09:51:06 -0700

    encoded string in 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337.
+ * @s: pointer to string
+ *
+ * Return: a pointer to resulting string
+ */
+char *leet(char *s)
+{
+	int a[11] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	int b[11] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (s[i] == a[j])
+				s[i] = b[j];
+		}
+	}
+	return (s);
+}
