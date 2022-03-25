71374be8d61f30c9fe939d87da12b299dc8affe3
junaideko <junaidekoraimot@gmail.com>
2022-03-24 23:47:24 -0700

    printed buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -22,13 +22,13 @@ void print_buffer(char *b, int size)
 			for (k = j; k < j + 10; k++)
 			{
 				if (k % 2 == 0)
-					printf(" ");
+					printf(" ");/*single space*/
 				if (k < size)
 					printf("%.2x", *(b + k));
 				else
-					printf(" ");
+					printf("  ");/*double space*/
 			}
-			printf(" ");
+			printf(" ");/*single space*/
 			for (l = j; l < j + 10; l++)
 			{
 				if (l >= size)
