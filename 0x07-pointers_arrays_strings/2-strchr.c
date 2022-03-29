f011b3ce2aff786efe27aab83483f8a9346bc0be
junaideko <junaidekoraimot@gmail.com>
2022-03-28 20:46:17 -0700

    located 1st ocuurence of char

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,18 +7,15 @@
  *
  * Return: Pointer to the character
 */
-
 char *_strchr(char *s, char c)
 {
+	int i = 0;
 
-	int i;
+	while (s[i] != '\0' && s[i] != c)
+		i++;
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] == c)
-			return (&s[i]);
-	}
-	if (c == '\0')
+	if (s[i] == c)
 		return (&s[i]);
-	return ('\0');
+	else
+		return (0);
 }
