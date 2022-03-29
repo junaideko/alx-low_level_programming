0632bc67e34ffd0d252e5b91290cfc349fcf7e88
junaideko <junaidekoraimot@gmail.com>
2022-03-29 08:17:48 -0700

    casted 2D array into a pointer

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - program prints the sum of the two
+ * diagonals of a square matrix of integers.
+ * @a: pointer to array
+ * @size: is an int
+ * Return: void
+ */
+void print_diagsums(int *a, int size)
+{
+	int sum1, sum2, i, max;
+
+	i = sum1 = sum2 = 0;
+	max = size * size;
+
+	while (i < max)
+	{
+		sum1 += a[i];
+		i = i + size + 1;
+	}
+
+	i = size - 1;
+	while (i < (max - 1))
+	{
+		sum2 += a[i];
+		i = i + (size - 1);
+	}
+
+	printf("%d, %d\n", sum1, sum2);
+}
