11b705305e6fc610f9ba9a539866ebc1eb1102e0
junaideko <junaidekoraimot@gmail.com>
2022-03-28 21:23:13 -0700

    copied n charcters from one mem to another

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *dest, char *src, usigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int_strspn(char *s, char *accept);
 
