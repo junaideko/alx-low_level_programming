df8673010cd7016c214593dafa578acbd4678704
junaideko <junaidekoraimot@gmail.com>
2022-03-30 22:30:56 -0700

    returned 1 for palindrome using recursion

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * c1 - function check for pallindrome.
+ * @s: is a pointer to string
+ * @l: is an integer
+ * Return: is an integer
+ */
+int c1(char *s, int l)
+{
+	if (l < 1)
+		return (1);
+
+	if (*s == *(s + l))
+		return (c1(s + 1, l - 2));
+	return (0);
+}
+/**
+ * is_palindrome - palindrome
+ * @s: pointer to string.
+ * Return: integer.
+ */
+int is_palindrome(char *s)
+{
+	int len = _strlen_recursion(s);
+
+	return (c1(s, len - 1));
+}
+
+/**
+ * _strlen_recursion - size
+ * @s: pointer to string params
+ * Return: integer
+ */
+int _strlen_recursion(char *s)
+{
+	if (!*s)
+		return (0);
+	return (1 + _strlen_recursion(++s));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,5 +10,7 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int divisors(int n, int m);
+int is_palindrome(char *s);
+int c1(char *s, int l);
 
 #endif
