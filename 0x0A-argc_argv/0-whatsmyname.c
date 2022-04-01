f552e2ec8e62124806b66608aa41c164119e04c8
junaideko <junaidekoraimot@gmail.com>
2022-04-01 14:43:18 -0700

    program printed its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - programs prints its name followed by a new line.
+ * @argc: counts for all agruments passed on the command line
+ * @argv: vector that points to and array of string.
+ * each strings corresponds to each argumants passed on the command line.
+ * Return: always 0
+ */
+int main(int argc, char **argv)
+{
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,13 @@
+Description:
+
+0-whatsmyname.c: program prints its name, followed by a new line.
+
+1-args.c: program prints the number of arguments passed into it.
+
+2-args.c: program prints all arguments it receives.
+
+3-mul.c: program multiplies two numbers.
+
+4-add.c: program adds positive numbers.
+
+100-change.c: program prints the minimum number of coins to make change for an amount of money.
