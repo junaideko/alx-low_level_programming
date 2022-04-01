3f62a2213dbca03f89db3169d9aa37bd59ad7c61
junaideko <junaidekoraimot@gmail.com>
2022-04-01 16:08:16 -0700

    multiplied two arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - programs prints its name followed by a new line.
+ * @argc: counts for all agruments passed on the command line
+ * @argv: vector that points to and array of string.
+ * each strings corresponds to each argumants passed on the command line.
+ * Return: always 0
+ */
+int main(int argc, char **argv)
+{
+	int product;
+
+	if (argc != 3)
+		puts("Error");
+	else
+	{
+		product = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", product);
+	}
+	return (0);
+}
