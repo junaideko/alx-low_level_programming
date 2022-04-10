091c2eed4a38d8b9e25fe412d217fbb8a0ccaa0e
junaideko <junaidekoraimot@gmail.com>
2022-04-10 07:19:49 -0700

    program multiplied two command line argument

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - function multiplies two positive numbers.
+ * @argc: is an integer
+ * @argv: is a pointer
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i, j;
+
+	unsigned long product;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > 57 || argv[i][j] < 48)
+			{
+				printf("Error\n");
+				exit(98);
+			}
+		}
+	}
+
+	product = atol(argv[1]) * atol(argv[2]);
+	printf("%lu\n", product);
+	return (0);
+}
