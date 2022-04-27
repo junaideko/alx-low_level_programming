2bba259dcd7300ec552cfaa00d9b87692d4780bf
junaideko <junaidekoraimot@gmail.com>
2022-04-26 22:13:55 -0700

    updated 3-add file

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -14,7 +14,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *newnode, *temphead;
 
-	newnode = malloc(sizeof(listint_t *);
+	newnode = malloc(sizeof(listint_t));
 	if (newnode == NULL)
 		return (NULL);
 
