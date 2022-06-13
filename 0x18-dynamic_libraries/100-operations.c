5a957f2266825bdcc88274b2a8e16b317b2c42f4
junaideko <junaidekoraimot@gmail.com>
2022-06-13 13:22:06 -0700

    dynamic library called by py

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+/**
+ * add - adds two integers
+ * @a: interger one
+ * @b: second interger
+ * Return: a whole integer of any of the operation
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - subtractc to ints
+ * @a: 1st int
+ * @b: 2nd int
+ * Return: sub of the two ints
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - mutiplies two int
+ * @a: 1st int
+ * @b: 2nd int
+ * Return: reult of mul
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - divides teo ints
+ * @a: 1ste int
+ * @b: 2nd int
+ * Return: whole number
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - divives and returns remainder onoy
+ * @a: 1st int
+ * @b: 2nd intr
+ * Return: remainder
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
